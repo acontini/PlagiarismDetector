8f5c780355f5747b596a3dd8e9a3a3458c2dc9fd
Ryan Demo <rdemo1@jhu.edu>
2016-12-06 23:09:18 -0500

    Added blank unit tests for PlagiarismDetector, edited Makefile

---
+++ src/unitTests-PlagiarismDetector.cpp
@@ -0,0 +1,37 @@
+/*
+600.120.01 CS120 Intermediate Programming
+Final Project - Plagiarism Detector
+unitTests-PlagiarismDetector.cpp - unit tests for the PlagiarismDetector class
+12/6/2016
+
+Ryan Demo
+rdemo1
+rdemo1@jhu.edu
+
+Mengying Pan
+mpan4
+mpan4@jhu.edu
+*/
+
+/*
+ * Unit test cases for PlagiarismDetector class.
+ *
+ * TODO: some test cases have been provided, but you will need
+ * to write your own for the remaining functions
+ */
+
+#include "catch.hpp" // simple unit-testing framework
+#include "NgramCollection.hpp" // header declaring the functions we want to test
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+
+using std::string;
+using std::vector;
+using std::list;
+
+
+
+
